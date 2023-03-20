b6e460ff07eaa619a33109efab9f0e1c05a2c3da
ChehST <chehstream@gmail.com>
2023-03-20 23:32:39 +0400

    Condition for DEBAG to import local envars

--- backend/dsam/settings.py
+++ backend/dsam/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import enviroment, os
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +26,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+if DEBUG:
+    import enviroment
 
 ALLOWED_HOSTS = ['http://localhost:3000',
     'localhost',
