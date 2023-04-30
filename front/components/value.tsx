8056b85fec79f0bd9dca05971f99e66c416dfc5c
ChehST <chehstream@gmail.com>
2023-04-30 07:33:32 +0400

    Исправил орфографию, помогла мама :)

--- front/components/value.tsx
+++ front/components/value.tsx
@@ -24,14 +24,14 @@ function Value() {
                                 <img src="/svg/re_man.svg" className="w-24 h-24" alt="Иконка человека, танцора"/>
                             </div>
                             <h2 className="text-center text-xl p-3 font-bold">Индивидуальный подход</h2>
-                            <p className="text-center">Каждому ребенку уделяется особых подход для комфортного обучения и развития</p>
+                            <p className="text-center">Каждому ребенку уделяется особый подход для комфортного обучения и развития</p>
                         </li>
                         <li className="my-6 lg:w-1/3">
                             <div className="flex justify-center">
                                 <img src="/svg/Event_agency_miracle.svg" className="w-24 h-24" alt="Магия танца. Волшебная палочка"/>
                             </div>
                             <h2 className="text-center text-xl p-3 font-bold">Становимся лучше</h2>
-                            <p className="text-center">Развитие новых физических и психоэмоциональных качеств. А именно: выносливость гибкость осанка, самоуверенность и другие</p>
+                            <p className="text-center">Развитие новых физических и психоэмоциональных качеств. А именно: выносливость, гибкость, осанка, самоуверенность и другие</p>
                         </li>
                     </ul>
                 </div>
