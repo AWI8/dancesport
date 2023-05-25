70c3df35b3193092717e4ed1c1d22e404b1d087f
ChehST <chehstream@gmail.com>
2023-05-25 13:08:53 +0400

    test(cypress): Small fixes to Form test

--- front/cypress/e2e/FormWorks.cy.tsx
+++ front/cypress/e2e/FormWorks.cy.tsx
@@ -2,7 +2,7 @@ describe('Feedback form', () => {
   it('submits form successfully', () => {
     cy.visit('/sing-in');
 
-    cy.get('input[name="name"]').type('autoTester');
+    cy.get('input[name="name"]').type('autoTesterFromCypress');
     cy.get('input[name="tel"]').type('88005553535');
     cy.get('form').submit();
     });
