b96feb0cd15cf0ed402a9638e21f23c370234a3e
ChehST <chehstream@gmail.com>
2023-06-07 10:08:39 +0400

    test(backend): add test for feedback api

---
+++ backend/tests/feedback_api_test.py
@@ -0,0 +1,31 @@
+import unittest
+import os
+
+import requests
+
+class APITestCase(unittest.TestCase):
+    base_url = 'http://127.0.0.1:8000/api_fdsam/'
+
+    def test_create_feedback(self):
+        url = self.base_url + 'feedback/create/'
+        token = os.environ.get('RECAPTCHASK')
+        print(token)
+        data = {
+            'name': 'TestCaseDEVlocal',
+            'phone_number': '1234567890',
+            'recaptcha_token': token ,
+        }
+
+        response = requests.post(url, data=data)
+        # Проверяем код состояния HTTP
+        self.assertEqual(response.status_code, 201)  # Ожидаем код 201 (Created)
+    
+        # Проверяем, что объект был создан успешно
+        feedback = response.json()
+        self.assertEqual(feedback['name'], 'TestCaseDEVlocal')
+        self.assertEqual(feedback['phone_number'], '1234567890')
+        
+    
+
+if __name__ == '__main__':
+    unittest.main()
